8a547a01457f8171137538335e517f17273b6e4e
boxp <tiyotiyouda@gmail.com>
2026-03-04 04:06:11 +0000

    fix: replace placeholder SHA256 with actual checksums for v0.0.8 tarballs

--- Formula/ceeker.rb
+++ Formula/ceeker.rb
@@ -10,19 +10,19 @@ class Ceeker < Formula
   on_macos do
     on_arm do
       url "https://github.com/boxp/ceeker/releases/download/v#{version}/ceeker-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "040c12266c750ea9cad8c5ef72baa37a2babbffaca5f3c1a84c88a752b6eaa65"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/boxp/ceeker/releases/download/v#{version}/ceeker-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "f9d9980088675c4146346174a457c27d6008de58fd4b63420e9c23ce7c562a9f"
     end
 
     on_intel do
       url "https://github.com/boxp/ceeker/releases/download/v#{version}/ceeker-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "f5175e0ab6075b8bd2eaebecbc1c7a864de945aca3943efb0757b896f8639ae1"
     end
   end
 
